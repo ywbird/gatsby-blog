0e410a251e5d7655437d96321dab6ea3ff0ed32f
ywbird <ywbird@naver.com>
2022-10-02 05:07:12 +0900

    code font fix

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,6 +1,6 @@
 const Fonts = {
   main: 'Noto Sans KR, Noto Sans CJK KR, arial, 돋움, Dotum, Tahoma, Geneva, sans-serif',
-  code: 'Source Code Pro, monospace',
+  code: 'Source Code Pro, Noto Sans KR, Noto Sans CJK KR, monospace',
 };
 
 export default Fonts;

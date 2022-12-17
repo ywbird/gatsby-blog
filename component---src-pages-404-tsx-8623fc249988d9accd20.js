37321d0f3bc5373d02c52b47521e370573f800a9
ywbird <ywbird@naver.com>
2022-12-17 23:32:53 +0900

    Updates

--- component---src-pages-404-tsx-8623fc249988d9accd20.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklotus=self.webpackChunklotus||[]).push([[218],{2513:function(n,e,u){u.r(e),u.d(e,{default:function(){return s}});var t=u(2212),r=u(5893);function s(){return(0,r.jsx)(t.Z,{pageTitle:"404",children:(0,r.jsx)("p",{children:"Sorry, page not found!"})})}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-8623fc249988d9accd20.js.map
